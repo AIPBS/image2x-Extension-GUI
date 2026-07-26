8a88e2ac84ce6485a6e0ff157c7b020b2e9f4067
AIPEAC <203755544+AIPEAC@users.noreply.github.com>
2026-07-27 01:01:05 +0800

    docs: add copyright notice to source files

--- SRC_v3.139.01-reconstructed/Waifu2x-Extension-QT/rife_v4_ncnn_vulkan.cpp
+++ SRC_v3.139.01-reconstructed/Waifu2x-Extension-QT/rife_v4_ncnn_vulkan.cpp
@@ -32,6 +32,35 @@
  * Command:        -i <input_dir> -o <output_dir> -m <model_dir> -g <gpu> -j <threads>
  */
 
+/*
+    Copyright (C) 2026  AIPEAC
+
+    This file is part of Waifu2x-Extension-GUI Reconstructed.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ADDITIONAL PERMISSION under GNU AGPL version 3 section 7:
+    As a special exception, the copyright holder of this file gives you
+    permission to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell this file without restriction, as though it were licensed
+    under a permissive license. This additional permission applies only to
+    this specific file and files explicitly marked with this notice.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Based on the original Waifu2x-Extension-GUI by Aaron Feng:
+    https://github.com/AaronFeng753/Waifu2x-Extension-GUI
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "platform_compat.h"

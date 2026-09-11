25a9819fa2bb9b970d2ffae89ac70824e255d444
AIPEAC <203755544+AIPEAC@users.noreply.github.com>
2026-09-11 06:26:09 +0000

    feat: restore Linux compatibility results release

--- src_code/image2x-Extension-QT/mainwindow.h
+++ src_code/image2x-Extension-QT/mainwindow.h
@@ -83,7 +83,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     void changeEvent(QEvent *e);
     //=======================
-    QString VERSION = "v0.3.2";
+    QString VERSION = "v0.3.5";
     bool isBetaVer = false;
     QString LastStableVer = "v3.31.13";
     QString LastBetaVer = "v3.41.02-beta";
